86dda6e5d7f9ad14dd3ef31fa8fac38c8ef07eba
Nicholas McKee <nmsmckee@gmail.com>
2022-12-06 00:58:24 -0500

    pep8 fixes i guess

--- Day 6/day6part1.py
+++ Day 6/day6part1.py
@@ -1,9 +1,8 @@
-
 if __name__ == "__main__":
     f = open("data6.txt")
     line = f.readline()
-    for i in range(len(line)-4):
-        subline = line[i:i+4]
+    for i in range(len(line) - 4):
+        subline = line[i:i + 4]
         chars = []
         for char in subline:
             if char in chars:
@@ -11,5 +10,5 @@ if __name__ == "__main__":
             else:
                 chars.append(char)
             if len(chars) == 4:
-                print(i+4)
+                print(i + 4)
                 quit()
--- Day 6/day6part2.py
+++ Day 6/day6part2.py
@@ -1,9 +1,8 @@
-
 if __name__ == "__main__":
     f = open("data6.txt")
     line = f.readline()
-    for i in range(len(line)-14):
-        subline = line[i:i+14]
+    for i in range(len(line) - 14):
+        subline = line[i:i + 14]
         chars = []
         for char in subline:
             if char in chars:
@@ -11,5 +10,5 @@ if __name__ == "__main__":
             else:
                 chars.append(char)
             if len(chars) == 14:
-                print(i+14)
+                print(i + 14)
                 quit()
