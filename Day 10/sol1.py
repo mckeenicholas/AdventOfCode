71b74d44dcafc87fc1071f3ee6d6e1fa6052501d
Nicholas McKee <nmsmckee@gmail.com>
2022-12-10 00:49:44 -0500

    Removed debugging stuff

--- Day 10/sol1.py
+++ Day 10/sol1.py
@@ -4,27 +4,16 @@ cycle = 1
 important_cycles = [20, 60, 100, 140, 180, 220]
 value_sum = 0
 for line in f:
-    if cycle == 180:
-        print("test")
     if line.strip() == "noop":
         if cycle in important_cycles:
-            print(value)
-            print(cycle)
-            print( (cycle * value))
             value_sum += (cycle * value)
         cycle += 1
     else:
         num = int(line.strip()[4:])
         if cycle in important_cycles:
-            print(value)
-            print(cycle)
-            print((cycle * value))
             value_sum += (cycle * value)
         cycle += 1
         if cycle in important_cycles:
-            print(value)
-            print(cycle)
-            print((cycle * value))
             value_sum += (cycle * value)
         cycle += 1
         value += num
