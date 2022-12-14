4894030220d9e0a2d3340eef63843b39a331d293
Nicholas McKee <nmsmckee@gmail.com>
2022-12-14 01:10:11 -0500

    AoC Day 14

---
+++ Day 14/sol1.py
@@ -0,0 +1,55 @@
+class Caves:
+    def __init__(self, cave):
+        self.cave = cave
+
+    def place(self, x, y):
+        if y < len(cave[0]) - 1:
+            if cave[x][y + 1] == ".":
+                return self.place(x, y + 1)
+            elif cave[x - 1][y + 1] == ".":
+                return self.place(x - 1, y + 1)
+            elif cave[x + 1][y + 1] == ".":
+                return self.place(x + 1, y + 1)
+            else:
+                self.cave[x][y] = "o"
+                return True
+        else:
+            return False
+
+    def __str__(self):
+        lines = ""
+        for i in range(len(cave[0])):
+            line = ""
+            for j in range(len(cave)):
+                line += cave[j][i]
+            lines += line + "\n"
+        return lines
+
+
+f = open("data14.txt")
+lines = [n.strip().split(" -> ") for n in f]
+cave = [["." for i in range(175)] for j in range(600)]
+for line in lines:
+    direction = []
+    for dir in line:
+        direction.append((int(dir.split(",")[0]), int(dir.split(",")[1])))
+    for i in range(len(direction) - 1):
+        for j in range(direction[i][0], direction[i + 1][0] + 1):
+            cave[j][direction[i][1]] = "#"
+        for j in range(direction[i][1], direction[i + 1][1] + 1):
+            cave[direction[i][0]][j] = "#"
+        for j in range(direction[i + 1][0], direction[i][0] + 1):
+            cave[j][direction[i][1]] = "#"
+        for j in range(direction[i + 1][1], direction[i][1] + 1):
+            cave[direction[i][0]][j] = "#"
+
+# Sand stuff
+full = False
+c = Caves(cave)
+
+while not full:
+    if not c.place(500, 0):
+        full = True
+
+print(c)
+print(c.__str__().count("o"))
